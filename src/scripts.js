47a96795ef3e227f2bbda8d09408c04253d2a1c4
Andrew Miller <andrew.r.miller45@gmail.com>
2022-08-01 17:21:22 -0700

    add api calls to repo

--- src/scripts.js
+++ src/scripts.js
@@ -2,10 +2,24 @@
 // Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
+import { getData } from './apicalls';
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+let listOfBookings
+let listOfCustomers
+let listOfRooms
+
+window.addEventListener('load', loadData)
+
+function loadData() {
+    Promise.all([getData('customers'), getData('rooms'), getData('bookings')]).then(data => {
+        listOfCustomers = data[0].customers;
+        listOfRooms = data[1].rooms;
+        listOfBookings = data[2].bookings;
+        console.log(listOfBookings);
+    })
+}
