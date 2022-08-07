e25bd9777869de360d6b85ec607b60648ee8932d
Andrew Miller <andrew.r.miller45@gmail.com>
2022-08-06 20:12:28 -0700

    render rooms not available for booking from page on click

--- src/scripts.js
+++ src/scripts.js
@@ -6,6 +6,8 @@ import { getData } from './apicalls'
 import './css/styles.css'
 import Customer from './classes/Customer.js'
 import Rooms from './classes/Rooms.js'
+import dayjs from 'dayjs'
+dayjs().format()
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/hotelroom.png'
@@ -17,20 +19,21 @@ let listOfRooms
 let customerClass
 let roomClass
 let managerClass
-
+let currentDate
 
 
 //q selectors 
-let roomContainer = document.querySelector('.room-container')
+let bookingsViewContainer = document.querySelector('.room-card-to-view')
+let viewAvailableRoomsContainer = document.querySelector('.room-card-to-book')
 // let userHeadlineMsg = document.querySelector('.')
 let submitButton = document.querySelector('#submitButton')
 let calendarValues = document.getElementById('calDate')
+let viewYourBookingsButton = document.querySelector('#bookingsButton')
 
 // e listeners 
 window.addEventListener('load', loadData)
-submitButton.addEventListener('click', (e) => {
-    console.log(calendarValues.value)
-})
+submitButton.addEventListener('click', displayAvailableRooms)
+viewYourBookingsButton.addEventListener('click', displayUsersBookings)
 
 //functions
 function loadData(  ) {
@@ -38,8 +41,8 @@ function loadData(  ) {
         listOfCustomers = data[0].customers
         listOfRooms = data[1].rooms
         listOfBookings = data[2].bookings
-        console.log(data)
         customerClass = new Customer(listOfCustomers[ Math.floor( Math.random( ) * listOfCustomers.length ) ])
+        roomClass = new Rooms(listOfRooms)
         customerClass.findBookings(listOfBookings)
         customerClass.updateCostPerNight(listOfRooms)
         customerClass.findSpendHistory()
@@ -48,10 +51,11 @@ function loadData(  ) {
 }
 
 function displayUsersBookings (  ) {
-    roomContainer.innerHTML = ""
+    hide(viewAvailableRoomsContainer)
+    show(bookingsViewContainer)
+    bookingsViewContainer.innerHTML = ""
     customerClass.bookings.map((booking => {
-        
-        return roomContainer.innerHTML += 
+        return bookingsViewContainer.innerHTML += 
         `<section class="room-card-to-view">
             <img src="./images/hotelroom.png" class="room-image" alt="room image" >
             <p>Date of Stay: ${booking.date}</p>
@@ -59,4 +63,32 @@ function displayUsersBookings (  ) {
             <p>Cost per Night: $${booking.amount}</p>
         </section>`
     }))
+}
+
+function displayAvailableRooms(e){
+    hide(bookingsViewContainer)
+    show(viewAvailableRoomsContainer)
+    let reformattedDate = dayjs(calendarValues.value).format("YYYY/MM/DD")
+    let availableRooms = roomClass.filterForAvailability(listOfBookings, reformattedDate)  
+        viewAvailableRoomsContainer.innerHTML = ""
+        availableRooms.map(availableRoom =>  {
+            console.log(availableRoom);
+            return viewAvailableRoomsContainer.innerHTML +=         
+            `<section class="room-card-to-book">
+                <img src="./images/hotelroom.png" class="room-image" alt="room image" >
+                <p>Room ${availableRoom.number}</p>
+                <p>Room Type: ${availableRoom.roomType}</p>
+                <p>${availableRoom.bedSize} size bed</p>
+                <p>Cost per Night: $${availableRoom.costPerNight}</p>
+                <button id="bookItButton">Book this room!</button>
+            </section>`
+        })
+}
+
+function hide(element){
+    element.classList.add('hidden')
+}
+
+function show(element){
+    element.classList.remove('hidden')
 }
